c92efe3a771837f233f5386d8c129d85ea29d9bf
benoitheintz <benoit.heintz@irisa.fr>
2016-07-20 17:15:40 +0200

    Update the demo for a real door, and add the motion to open and close the door

--- src/demo_romeo_robot_door.cpp
+++ src/demo_romeo_robot_door.cpp
@@ -112,8 +112,8 @@ void DemoRomeoDoor::spin()
     {
       //OpenLoop Control to see the door handle
       head_pose = 0;
-      head_pose[0] = vpMath::rad(-39.0); // NeckYaw
-      head_pose[1] = vpMath::rad(15.0); // NeckPitch
+      head_pose[0] = vpMath::rad(-24.3); // NeckYaw
+      head_pose[1] = vpMath::rad(20.2); // NeckPitch
       head_pose[2] = vpMath::rad(-7.6); // HeadPitch
       head_pose[3] = vpMath::rad(0.0); // HeadRoll
       romeo.setPosition(jointNamesHead, head_pose, 0.06);
@@ -144,6 +144,7 @@ void DemoRomeoDoor::spin()
         vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to servo the RArm near the door handle", vpColor::red);
 
       if (click_done && button == vpMouseButton::button1 && once == 0) {
+//        state = GoBacktoResPosition;
         moveRArmFromRestPosition(); // move up
         once = 1;
         click_done = false;
@@ -185,7 +186,7 @@ void DemoRomeoDoor::spin()
         pbvs_active_pub.publish(pbvs_active);
         vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to put the hand on the door handle", vpColor::red);
         if (click_done && button == vpMouseButton::button1 ) {
-          state = OpenDoor1;
+          state = PutHandOnDoorHandle1;
           click_done = false;
         }
         ROS_INFO("Right Arm should have finished to be servoed");
@@ -196,12 +197,12 @@ void DemoRomeoDoor::spin()
 
 
     }
-    if (state == OpenDoor1)
+    if (state == PutHandOnDoorHandle1)
     {
       // Open loop upward motion of the hand
       vpColVector cart_delta_pos(6, 0);
 //      cart_delta_pos[1] = 0.02;
-      cart_delta_pos[3] = vpMath::rad(-80);
+      cart_delta_pos[3] = vpMath::rad(-5);
       double delta_t = 3;
 
 
@@ -217,46 +218,48 @@ void DemoRomeoDoor::spin()
         romeo.stop(moveCartesian.getJointNames());
         vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to do next step", vpColor::red);
         if (click_done && button == vpMouseButton::button1 ) {
-          state = OpenDoor2;
+          state = PutHandOnDoorHandle2;
           click_done = false;
         }
         ROS_INFO("Right Arm should have finished to open the door");
       }
 
     }
-    if (state == OpenDoor2)
+    if (state == PutHandOnDoorHandle2)
     {
-      if (!once)
-      {
-        // Open loop upward motion of the hand
-        vpColVector cart_delta_pos(6, 0);
-        cart_delta_pos[5] = vpMath::rad(-20);
-        //      cart_delta_pos[0] = 0.04;
-        //      cart_delta_pos[1] = 0.065;
-        //      cart_delta_pos[2] = +0.015;
-        double delta_t = 3;
+//      if (!once)
+//      {
+//        // Open loop upward motion of the hand
+//        vpColVector cart_delta_pos(6, 0);
+//        //      cart_delta_pos[0] = 0.04;
+//        //      cart_delta_pos[1] = 0.065;
+//        //      cart_delta_pos[2] = +0.015;
+//        double delta_t = 3;
 
 
-        static vpCartesianDisplacement moveCartesian;
-        vpVelocityTwistMatrix V;
-        if (moveCartesian.computeVelocity(romeo, cart_delta_pos, delta_t, "RArm", V)) {
-          romeo.setVelocity(moveCartesian.getJointNames(), moveCartesian.getJointVelocity());
-          //        ROS_INFO("Right Arm should go in openLoop to open the door");
-        }
-        else
-        {
-          romeo.stop(moveCartesian.getJointNames());
-          vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to grasp the door handle", vpColor::red);
-          once = 1;
-          ROS_INFO("Right Arm should have put the hand on the door handle");
-        }
-      }
-      else if (once == 1) {
+//        static vpCartesianDisplacement moveCartesian;
+//        vpVelocityTwistMatrix V;
+//        if (moveCartesian.computeVelocity(romeo, cart_delta_pos, delta_t, "RArm", V)) {
+//          romeo.setVelocity(moveCartesian.getJointNames(), moveCartesian.getJointVelocity());
+//          //        ROS_INFO("Right Arm should go in openLoop to open the door");
+//        }
+//        else
+//        {
+//          romeo.stop(moveCartesian.getJointNames());
+//          vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to grasp the door handle", vpColor::red);
+//          once = 1;
+//          ROS_INFO("Right Arm should have put the hand on the door handle");
+//        }
+//      }
+//      else if (once == 1) {
 
         // Second open loop upward motion of the hand
         vpColVector cart_delta_pos2(6, 0);
 //        cart_delta_pos2[0] = 0.04;
-        cart_delta_pos2[1] = 0.065;
+//        cart_delta_pos2[0] = 0.02;
+        cart_delta_pos2[5] = vpMath::rad(-5);
+        cart_delta_pos2[1] = 0.11;
+        cart_delta_pos2[2] = -0.025;
         double delta_t2 = 3;
 
 
@@ -277,14 +280,14 @@ void DemoRomeoDoor::spin()
           }
           ROS_INFO("Right Arm should have put the hand on the door handle");
         }
-      }
+//      }
 
     }
     if (state == GraspingDoorHandle)
     {
       romeo.getProxy()->setStiffnesses("RHand", 1.0f);
       AL::ALValue angle = 0.50;
-      romeo.getProxy()->setAngles("RHand", angle, 0.15);
+      romeo.getProxy()->setAngles("RHand", angle, 0.30);
       vpTime::sleepMs(2000);
       state = PutTheHandCloser;
       ROS_INFO("Right Arm should have grasped the handle");
@@ -314,7 +317,7 @@ void DemoRomeoDoor::spin()
     {
       romeo.getProxy()->setStiffnesses("RHand", 1.0f);
       AL::ALValue angle = 0.30;
-      romeo.getProxy()->setAngles("RHand", angle, 0.15);
+      romeo.getProxy()->setAngles("RHand", angle, 0.30);
       vpTime::sleepMs(2000);
       state = PutTheHandCloser2;
       ROS_INFO("Right Arm should have grasped the handle");
@@ -324,6 +327,7 @@ void DemoRomeoDoor::spin()
       // Open loop upward motion of the hand
       vpColVector cart_delta_pos(6, 0);
       cart_delta_pos[0] = 0.02;
+      cart_delta_pos[1] = 0.01;
       double delta_t = 3;
 
 
@@ -345,20 +349,21 @@ void DemoRomeoDoor::spin()
       if (!once) {
         romeo.getProxy()->setStiffnesses("RHand", 1.0f);
         AL::ALValue angle = 0.15;
-        romeo.getProxy()->setAngles("RHand", angle, 0.15);
+        romeo.getProxy()->setAngles("RHand", angle, 0.30);
         vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to rotate the door handle", vpColor::red);
-        vpTime::sleepMs(2000);
+        vpTime::sleepMs(1000);
         once = 1;
       }
+      vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to rotate the door handle", vpColor::red);
       if (click_done && button == vpMouseButton::button1 ) {
         state = RotatingHandle;
         once = 0;
         click_done = false;
       }
-      ROS_INFO("Right Arm should have grasped the handle");
+//      ROS_INFO("Right Arm should have grasped the handle");
     }
     if (state == RotatingHandle)
-    {
+    {/*
       vpHomogeneousMatrix hMp;
       for (int i = 0; i < 3; i++)
         hMp[i][i] = 0;
@@ -366,56 +371,174 @@ void DemoRomeoDoor::spin()
       hMp[1][0] = -1;
       hMp[2][1] = 1;
       hMp[0][2] = -1;
-      hMp[0][3] = 0.15;
+      hMp[0][3] = 0.20;  //15
       hMp[1][3] = 0.05;
       hMp[2][3] = -0.02;
+
 
       vpVelocityTwistMatrix wVh(hMp);
       vpColVector v_wrist, v_handle(6,0);
       v_handle[5] = -vpMath::rad(10);
       v_wrist = wVh * v_handle;
+      ROS_INFO_STREAM("hMp" << hMp << "wVh" << wVh << "v_wrist" << v_wrist);
       static double t_1 = vpTime::measureTimeSecond();
-      if ( vpTime::measureTimeSecond() - t_1 < 7.0)
+      if ( vpTime::measureTimeSecond() - t_1 < 6.0)
       {
          vpColVector q = romeo.get_eJe("RArm").pseudoInverse() * v_wrist;
 //         ROS_INFO_STREAM("q = " << q << " v_wrist = " << v_wrist);
          std::vector<std::string> jointNames = romeo.getBodyNames("RArm");
          jointNames.pop_back(); // We don't consider  the last joint of the arm = Hand
          romeo.setVelocity(jointNames, q);
+      }*/
+
+      // Open loop upward motion of the hand
+      vpColVector cart_delta_pos(6, 0);
+      cart_delta_pos[3] = vpMath::rad(+40);
+      cart_delta_pos[2] = -0.07;
+      double delta_t = 3;
+
+
+      static vpCartesianDisplacement moveCartesian;
+      vpVelocityTwistMatrix V;
+      if (moveCartesian.computeVelocity(romeo, cart_delta_pos, delta_t, "RArm", V)) {
+        romeo.setVelocity(moveCartesian.getJointNames(), moveCartesian.getJointVelocity());
+//        ROS_INFO("Right Arm should go in openLoop to open the door");
       }
+
       else
       {
         romeo.stop(jointNames_tot);
+        vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to open the door", vpColor::red);
         if (click_done && button == vpMouseButton::button1 ) {
-          state = ReleaseDoorHandle;
+          state = OpenDoor;
           click_done = false;
         }
         ROS_INFO("Right Arm should have opened the door");
       }
     }
+    if (state == OpenDoor)
+    {
+      // Open loop upward motion of the hand
+      vpColVector cart_delta_pos(6, 0);
+      cart_delta_pos[0] = -0.10;
+      cart_delta_pos[1] = -0.03;
+      double delta_t = 3;
+
+
+      static vpCartesianDisplacement moveCartesian;
+      vpVelocityTwistMatrix V;
+      if (moveCartesian.computeVelocity(romeo, cart_delta_pos, delta_t, "RArm", V)) {
+        romeo.setVelocity(moveCartesian.getJointNames(), moveCartesian.getJointVelocity());
+//        ROS_INFO("Right Arm should go in openLoop to open the door");
+      }
+      else
+      {
+        romeo.stop(moveCartesian.getJointNames());
+        vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to close the door", vpColor::red);
+        if (click_done && button == vpMouseButton::button1 ) {
+          state = CloseDoor;
+          click_done = false;
+        }
+        ROS_INFO("Right Arm should have finished to open the door");
+      }
+    }
+    if (state == CloseDoor)
+    {
+      // Open loop upward motion of the hand
+      vpColVector cart_delta_pos(6, 0);
+      cart_delta_pos[0] = 0.10;
+      cart_delta_pos[1] = 0.03;
+      double delta_t = 3;
+
+
+      static vpCartesianDisplacement moveCartesian;
+      vpVelocityTwistMatrix V;
+      if (moveCartesian.computeVelocity(romeo, cart_delta_pos, delta_t, "RArm", V)) {
+        romeo.setVelocity(moveCartesian.getJointNames(), moveCartesian.getJointVelocity());
+//        ROS_INFO("Right Arm should go in openLoop to open the door");
+      }
+      else
+      {
+        romeo.stop(moveCartesian.getJointNames());
+        vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to release the handle", vpColor::red);
+        if (click_done && button == vpMouseButton::button1 ) {
+          state = ReleaseDoorHandle;
+          click_done = false;
+        }
+        ROS_INFO("Right Arm should have finished to open the door");
+      }
+    }
+
     if (state == ReleaseDoorHandle)
     {
       romeo.getProxy()->setStiffnesses("RHand", 1.0f);
-      AL::ALValue angle = 1.00;
+      AL::ALValue angle = 0.25;
       romeo.getProxy()->setAngles("RHand", angle, 0.15);
-      if (click_done && button == vpMouseButton::button1 ) {
+//      vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to go away from the handle", vpColor::red);
+//      if (click_done && button == vpMouseButton::button1 ) {
         state = GoBacktoResPosition;
-        click_done = false;
-      }
+//        click_done = false;
+//      }
       ROS_INFO("Right Arm should have released the handle");
+    }
+    if (state == GetAwayFromHandle)
+    {
+      std::vector<float> RArmElbowWrist_pos;
+      std::vector<std::string> RArmElbowWrist_name;
+      RArmElbowWrist_name.push_back("RElbowYaw");
+      RArmElbowWrist_name.push_back("RWristPitch");
+
+      RArmElbowWrist_pos = romeo.getProxy()->getAngles(RArmElbowWrist_name, true);
+
+      RArmElbowWrist_pos[0] -= vpMath::rad(+11.);
+      RArmElbowWrist_pos[1] -= vpMath::rad(+5.);
+
+      romeo.getProxy()->setAngles(RArmElbowWrist_name, RArmElbowWrist_pos, 0.02);
+
+      once = 0;
+
+//      vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to quit the demo", vpColor::red);
+//      if (click_done && button == vpMouseButton::button1 ) {
+        state = GoBacktoResPosition;
+//        click_done = false;
+//      }
+      ROS_INFO("Right Arm should have finished to open the door");
     }
     if (state == GoBacktoResPosition)
     {
-      moveRArmToRestPosition(); // move up
-      ROS_INFO("Right Arm should go back in rest position");
-      state = DoorOpenedAndQuit;
+//      ROS_INFO("Right Arm should go in zero position");
+      if (once == 0)
+        vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to put back the RArm in rest position", vpColor::red);
+      else
+        vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to open the fingers", vpColor::red);
+
+      if (click_done && button == vpMouseButton::button1 && once == 0) {
+        moveRArmToRestPosition();
+        once = 1;
+        click_done = false;
+      }
+      if (click_done && button == vpMouseButton::button1 && once == 1) {
+        romeo.getProxy()->setStiffnesses("RHand", 1.0f);
+        AL::ALValue angle = 1.0;
+        romeo.getProxy()->setAngles("RHand", angle, 1.);
+        state = DoorOpenedAndQuit;
+        once = 0;
+        click_done = false;
+      }
+
     }
+
     if (state == DoorOpenedAndQuit)
     {
       state_door_handle_sub.shutdown();
       state_hand_sub.shutdown();
       pbvs_finished_sub.shutdown();
-      break;
+      vpDisplay::displayText(img_, vpImagePoint(15,10), "Left click to quit the demo", vpColor::red);
+      if (click_done && button == vpMouseButton::button1 ) {
+        break;
+        click_done = false;
+      }
+
     }
 
     ros::spinOnce();
@@ -448,9 +571,9 @@ void DemoRomeoDoor::moveRArmFromRestPosition ()
   try
   {
 
-    AL::ALValue pos1 = AL::ALValue::array(0.23939552903175354, -0.3148501515388489, -0.2502657473087311, 1.7767317295074463, 1.0605800151824951, 0.25196322798728943);
-    AL::ALValue pos2 = AL::ALValue::array(0.32969486713409424, -0.4350138306617737, -0.006048134993761778, 1.3860777616500854, 0.1138603538274765, -0.2591860890388489);
-    AL::ALValue pos3 = AL::ALValue::array(0.3154124319553375, -0.3724079132080078, 0.10410818457603455, 1.441423773765564, -0.32187700271606445, 0.1536785364151001);
+//    AL::ALValue pos1 = AL::ALValue::array(0.20587599277496338, -0.4126491844654083, -0.18764731287956238, 1.0564998388290405, 0.6922455430030823, -0.44560280442237854);
+//    AL::ALValue pos2 = AL::ALValue::array(0.29286253452301025, -0.38179177045822144, -0.09054779261350632, 1.2106484174728394, 0.13958579301834106, -0.011433127336204052);
+//    AL::ALValue pos3 = AL::ALValue::array(0.3158690333366394, -0.3941819667816162, 0.0980803444981575, 1.2527700662612915, -0.028855890035629272, 0.19667623937129974);
 
     //New ones
 //    AL::ALValue pos1 = AL::ALValue::array(0.2581080198287964, -0.371820867061615, -0.17735163867473602, 1.582729697227478, 0.5320466756820679, 0.17236517369747162);
@@ -458,9 +581,9 @@ void DemoRomeoDoor::moveRArmFromRestPosition ()
 //    AL::ALValue pos3 = AL::ALValue::array(0.31288379430770874, -0.32327330112457275, 0.038474030792713165, 1.4297363758087158, -0.06524830311536789, 0.14893878996372223);
 
 //    AL::ALValue pos3 = AL::ALValue::array(0.3689846992492676, -0.20184889435768127, 0.08642885833978653, 1.496846318244934, -0.3719552457332611, 0.3171626925468445);
-//    AL::ALValue pos1 = AL::ALValue::array(0.3741794228553772, -0.33311545848846436, -0.036883752793073654, 1.260278582572937, 0.4322237968444824, 0.009434251114726067);
-//    AL::ALValue pos2 = AL::ALValue::array(0.39718443155288696, -0.282814621925354, 0.17343932390213013, 1.194741129875183, -0.5093367099761963, 0.18652880191802979);
-//    AL::ALValue pos3 = AL::ALValue::array(0.35522401332855225, -0.1643453985452652, 0.13889043033123016, 1.400763988494873, -0.566399872303009, 0.49108603596687317);
+    AL::ALValue pos1 = AL::ALValue::array(0.3741794228553772, -0.33311545848846436, -0.036883752793073654, 1.260278582572937, 0.4322237968444824, 0.009434251114726067);
+    AL::ALValue pos2 = AL::ALValue::array(0.39718443155288696, -0.282814621925354, 0.17343932390213013, 1.194741129875183, -0.5093367099761963, 0.18652880191802979);
+    AL::ALValue pos3 = AL::ALValue::array(0.3460591435432434, -0.2849748432636261, 0.08855552971363068, 0.9453462958335876, -0.11968476325273514, 0.25619229674339294);
 
     AL::ALValue time1 = 1.5f;
     AL::ALValue time2 = 3.0f;
@@ -502,13 +625,15 @@ void DemoRomeoDoor::moveRArmToRestPosition ()
   try
   {
 
-    AL::ALValue pos1 = AL::ALValue::array(0.37073051929473877, -0.32853859663009644, 0.153438001871109, 1.3140619993209839, -0.228091761469841, 0.3736579716205597);
-    AL::ALValue pos2 = AL::ALValue::array(0.2739490866661072, -0.3648371994495392, -0.20864969491958618, 1.6371514797210693, 0.5508617162704468, 0.19728609919548035);
-    AL::ALValue pos3 = AL::ALValue::array(0.08025781065225601, -0.20469778776168823, -0.32411444187164307, 1.9427353143692017, 1.112269639968872, 0.6806193590164185);
+    AL::ALValue pos1 = AL::ALValue::array(0.36160808801651, -0.14481818675994873, -0.012668244540691376, 0.9340378642082214, -0.3044275641441345, 0.25746822357177734);
+    AL::ALValue pos2 = AL::ALValue::array(0.3901706337928772, -0.26107993721961975, -0.012815611436963081, 0.9594348669052124, 0.11563336104154587, 0.38348156213760376);
+    AL::ALValue pos3 = AL::ALValue::array(0.2876889705657959, -0.27668479084968567, -0.22870546579360962, 0.8602628111839294, 0.8492440581321716, 0.10930496454238892);
+    AL::ALValue pos4 = AL::ALValue::array(0.117273710668087, -0.22325754165649414, -0.3122972846031189, 1.6027156114578247, 1.20582115650177, 0.5312549471855164);
 
-    AL::ALValue time1 = 1.5f;
-    AL::ALValue time2 = 3.0f;
-    AL::ALValue time3 = 5.0f;
+    AL::ALValue time1 = 2.0f;
+    AL::ALValue time2 = 4.0f;
+    AL::ALValue time3 = 6.0f;
+    AL::ALValue time4 = 8.0f;
 
 
 
@@ -516,12 +641,14 @@ void DemoRomeoDoor::moveRArmToRestPosition ()
     path.arrayPush(pos1);
     path.arrayPush(pos2);
     path.arrayPush(pos3);
+    path.arrayPush(pos4);
 
 
     AL::ALValue times;
     times.arrayPush(time1);
     times.arrayPush(time2);
     times.arrayPush(time3);
+    times.arrayPush(time4);
 
     AL::ALValue chainName  = AL::ALValue::array ("RArm");
     AL::ALValue space      = AL::ALValue::array (0); // Torso
